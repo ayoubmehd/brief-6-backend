7d340723010318daa1a041e76d8d18b0e2b6ed91
ayoubmehd <safi-_-gio-raja@live.fr>
2021-06-16 21:57:30 +0100

    chore: add header to allow all origin for devlopment

--- api/index.php
+++ api/index.php
@@ -1,4 +1,12 @@
 <?php
 require_once __DIR__ . "/../config/router.php";
 
-echo json_encode(router("api"));
+$router = router("api");
+
+$app = $router["app"];
+$app->set_header("Access-Control-Allow-Origin", "*");
+$app->set_header("Content-Type", "application/json");
+$app->set_header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+$app->set_header("Access-Control-Allow-Headers", "Origin, Content-Type");
+
+echo json_encode($router["action"]);
