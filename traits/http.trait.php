ad5c26e785df29bf52247e14a201778c7cdffd4a
ayoubmehd <safi-_-gio-raja@live.fr>
2021-06-16 20:34:56 +0100

    chore: handle http request inside Http trait check (request type get request body)

--- traits/http.trait.php
+++ traits/http.trait.php
@@ -6,4 +6,33 @@ trait Http
     {
         header("Location: " . BASE_URL . $path);
     }
+
+    public function requestMethod()
+    {
+        return $_SERVER["REQUEST_METHOD"];
+    }
+    public function isGet()
+    {
+        return $this->requestMethod() === "GET";
+    }
+    public function isPost()
+    {
+        return $this->requestMethod() === "POST";
+    }
+    public function isPut()
+    {
+        return $this->requestMethod() === "PUT";
+    }
+    public function isDelete()
+    {
+        return $this->requestMethod() === "DELETE";
+    }
+
+    public function request($prop = "")
+    {
+        $body = json_decode(file_get_contents("php://input"));
+        if ($prop === "") return $body;
+
+        return $body->$prop;
+    }
 }
