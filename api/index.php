1e5af2995a612ca417acefb28909a6a473682c01
ayoubmehd <safi-_-gio-raja@live.fr>
2021-06-20 22:39:58 +0100

    chorr(cors): allow post put delete

--- api/index.php
+++ api/index.php
@@ -6,7 +6,7 @@ $router = router("api");
 $app = $router["app"];
 $app->set_header("Access-Control-Allow-Origin", "http://localhost:8080");
 $app->set_header("Content-Type", "application/json; charset=UTF-8");
-$app->set_header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, PATCH, DELETE");
-$app->set_header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, content-type, Accept");
+$app->set_header("Access-Control-Allow-Methods", "POST, PUT, DELETE");
+$app->set_header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
 
 echo json_encode($router["action"]);
